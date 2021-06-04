e4002d780324b1949b7ed01629a44e5b608c0960
Marinessa393 <futurism1303@gmail.com>
2021-06-04 23:00:05 +0300

    done

---
+++ contacts.js
@@ -0,0 +1,57 @@
+const fs = require("fs");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+function listContacts() {
+  fs.readFile(contactsPath, (error, data) => {
+    if (error) throw error;
+    console.table(JSON.parse(data));
+  });
+}
+
+function getContactById(contactId) {
+  fs.readFile(contactsPath, (error, data) => {
+    if (error) throw error;
+    const contacts = JSON.parse(data);
+    const contact = contacts.find((id) => contactId === id);
+    if (!contact) console.log("Oops, not found :(");
+    console.table(contact);
+  });
+}
+
+function removeContact(contactId) {
+  fs.readFile(contactsPath, (error, data) => {
+    if (error) throw error;
+    const contacts = JSON.parse(data);
+    const refreshedContacts = contacts.filter((id) => contactId !== id);
+    if (contacts.length !== refreshedContacts.length) {
+      rewriteData(contactsPath, refreshedContacts);
+    }
+    console.table(refreshedContacts);
+  });
+}
+
+function addContact(name, email, phone) {
+  fs.readFile(contactsPath, (error, data) => {
+    if (error) throw error;
+    const contacts = JSON.parse(data);
+    const newContact = { id: uuidv4, name, email, phone };
+    const newContactsArray = [...contacts, newContact];
+    rewriteData(contactsPath, newContactsArray);
+    console.table(newContactsArray);
+  });
+}
+
+function rewriteData(path, array) {
+  const data = JSON.stringify(array);
+  fs.writeFile(path, data, (error) => {
+    if (error) {
+      console.log(error.message);
+      return;
+    }
+  });
+}
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
